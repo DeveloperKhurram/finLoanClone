f6c8ea1120e38543be55c6a583a3be0ee9491775
SheikhAbdullahSoft <audiobook.urdunovels@gmail.com>
2024-10-20 13:24:14 +0500

    basic css & js files added

---
+++ js/main.js
@@ -0,0 +1,41 @@
+AOS.init({
+  duration: 1500, 
+  once: true,
+});
+
+const dropdowns = document.querySelectorAll(".nav-item.dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const dropdownMenu = dropdown.querySelector(".dropdown-menu");
+  let hideDropdownTimeout;
+
+  dropdown.addEventListener("mouseenter", function () {
+    clearTimeout(hideDropdownTimeout);
+    dropdownMenu.style.display = "block";
+    dropdownMenu.style.opacity = "1";
+    dropdownMenu.style.visibility = "visible";
+    dropdownMenu.style.transform = "translateY(20px)";
+  });
+
+  dropdown.addEventListener("mouseleave", function () {
+    hideDropdownTimeout = setTimeout(function () {
+      dropdownMenu.style.display = "none";
+      dropdownMenu.style.opacity = "0";
+      dropdownMenu.style.visibility = "hidden";
+      dropdownMenu.style.transform = "translateY(40px)";
+    }, 200);
+  });
+});
+
+$(document).ready(function () {
+  $("select").niceSelect();
+
+  if ($.fn.slicknav) {
+    $('#menu').slicknav({
+       label: '',
+       duration: 800,
+    });
+ } else {
+    console.error('SlickNav is not initialized');
+ }
+});
